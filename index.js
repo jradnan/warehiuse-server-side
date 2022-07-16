7f3b3c9ea62fbd65097966ca00b5b39c766d24f3
jradnan <mohd.yasin085208@gmail.com>
2022-07-16 20:19:08 +0600

    server-connected

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion ,ObjectId } = require('mongodb');
+
+// All imports End here
+
+const app = express()
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT|| 5000 ;
+
+
+
+// mongodb connection uri
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.viapzy5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db('inventory').collection('product');
+
+        // get product
+        app.get('/product', async(req, res) =>{
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        app.get('/product/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        });
+
+        // POST product
+        app.post('/product', async(req, res) =>{
+            const newproduct = req.body;
+            console.log('adding new user', newproduct);
+            const result = await productCollection.insertOne(newproduct);
+            res.send(result)
+        });
+
+        // update product
+        app.put('/product/:id', async(req, res) =>{
+            const id = req.params.id;
+            const updatedProduct = req.body;
+            console.log(updatedProduct)
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+
+                  quantity: updatedProduct.quantity
+
+                }
+            };
+            const result = await productCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+
+        })
+
+        // delete a user
+        app.delete('/product/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) =>{
+    res.send('Inventory Management Server Running');
+});
+
+app.listen(PORT, () =>{
+    console.log(`Inventory Management Server Running PORT ${PORT}`);
+})
